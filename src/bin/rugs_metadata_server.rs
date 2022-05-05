319c2d02d490f45250e99d012559b8e953b970b3
Jørgen P. Tjernø <jorgen@tjer.no>
2022-05-05 12:57:24 -0700

    Add initial error handling for db errors etc

--- src/bin/rugs_metadata_server.rs
+++ src/bin/rugs_metadata_server.rs
@@ -1,8 +1,9 @@
+use anyhow::anyhow;
 use axum::{
     extract::Query,
     http::{self, Request, StatusCode, Uri},
     middleware::{self, Next},
-    response::{ErrorResponse, IntoResponse},
+    response::{ErrorResponse, IntoResponse, Response},
     routing::{get, post},
     Extension, Json, Router,
 };
@@ -11,11 +12,26 @@ use serde::Deserialize;
 use sqlx::SqlitePool;
 use tower::ServiceBuilder;
 use tower_http::trace::TraceLayer;
-use tracing::info;
+use tracing::{error, info};
 
 use std::{error::Error, fs::File, io::BufReader, net::SocketAddr, num::NonZeroI64, path::Path};
 
 use rugs::models::*;
+
+#[derive(Debug)]
+pub struct AppError(anyhow::Error);
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, self.0.to_string()).into_response()
+    }
+}
+
+impl<E: Into<anyhow::Error>> From<E> for AppError {
+    fn from(err: E) -> Self {
+        Self(err.into())
+    }
+}
 
 fn read_config_from_file<P: AsRef<Path>>(path: P) -> Result<Config, Box<dyn Error>> {
     let file = File::open(path)?;
@@ -131,21 +147,20 @@ struct LatestParams {
 async fn latest(
     Extension(pool): Extension<SqlitePool>,
     params: Query<LatestParams>,
-) -> impl IntoResponse {
+) -> Result<impl IntoResponse, AppError> {
     let row = sqlx::query!(
         "SELECT id FROM badges INNER JOIN projects USING(project_id) WHERE project = ? ORDER BY id DESC LIMIT 1",
         params.project
     )
     .fetch_optional(&pool)
-    .await
-    .unwrap();
+    .await?;
 
     let response = LatestResponseV1 {
         last_build_id: row.map_or(0, |row| row.id),
         last_comment_id: 0,
         last_event_id: 0,
     };
-    (StatusCode::OK, Json(response))
+    Ok((StatusCode::OK, Json(response)))
 }
 
 #[derive(Debug, Deserialize)]
@@ -157,7 +172,7 @@ struct BadgesParams {
 async fn badges(
     Extension(pool): Extension<SqlitePool>,
     params: Query<BadgesParams>,
-) -> impl IntoResponse {
+) -> Result<impl IntoResponse, AppError> {
     info!(
         "project: {}, build id: {}",
         params.project, params.lastbuildid
@@ -167,41 +182,50 @@ async fn badges(
         params.lastbuildid,
         params.project
     )
-    .map(|row| Badge {
-        id: NonZeroI64::new(row.id),
-        change_number: row.change_number,
-        added_at: chrono::DateTime::<chrono::Utc>::from_utc(row.added_at, chrono::Utc),
-        build_type: row.build_type,
-        result: BuildDataResult::from_i64(row.result).unwrap(),
-        url: row.url,
-        project: row.project,
-        archive_path: row.archive_path,
+    .map(|row| -> Result<Badge, AppError> {
+        Ok(Badge {
+            id: NonZeroI64::new(row.id),
+            change_number: row.change_number,
+            added_at: chrono::DateTime::<chrono::Utc>::from_utc(row.added_at, chrono::Utc),
+            build_type: row.build_type,
+            result: BuildDataResult::from_i64(row.result).ok_or_else(|| anyhow!("Invalid build data result in db for {}", row.id))?,
+            url: row.url,
+            project: row.project,
+            archive_path: row.archive_path,
+        })
     })
     .fetch_all(&pool)
-    .await
-    .unwrap();
+    .await?;
 
-    (StatusCode::OK, Json(response))
+    let (badges, errors): (Vec<_>, Vec<_>) = response.into_iter().partition(|r| r.is_ok());
+
+    for error in errors {
+        error!("bad badge in database: {}", error.unwrap_err().0);
+    }
+
+    let badges: Vec<Badge> = badges.into_iter().map(|badge| badge.unwrap()).collect();
+
+    Ok((StatusCode::OK, Json(badges)))
 }
 
-async fn get_or_add_project(pool: &SqlitePool, project_name: &str) -> i64 {
+async fn get_or_add_project(pool: &SqlitePool, project_name: &str) -> Result<i64, AppError> {
     let record = sqlx::query!(
         "SELECT project_id FROM projects WHERE project = ? LIMIT 1",
         project_name
     )
     .fetch_optional(pool)
-    .await
-    .unwrap();
+    .await?;
 
     if let Some(record) = record {
-        record.project_id
+        Ok(record.project_id)
     } else {
         // TODO: Thread safe
-        sqlx::query!("INSERT INTO projects (project) VALUES (?)", project_name)
-            .execute(pool)
-            .await
-            .unwrap()
-            .last_insert_rowid()
+        Ok(
+            sqlx::query!("INSERT INTO projects (project) VALUES (?)", project_name)
+                .execute(pool)
+                .await?
+                .last_insert_rowid(),
+        )
     }
 }
 
@@ -226,8 +250,8 @@ async fn issues() -> impl IntoResponse {
 async fn add_badge(
     Extension(pool): Extension<SqlitePool>,
     Json(badge): Json<CreateBadge>,
-) -> impl IntoResponse {
-    let project_id = get_or_add_project(&pool, &badge.project).await;
+) -> Result<impl IntoResponse, AppError> {
+    let project_id = get_or_add_project(&pool, &badge.project).await?;
     let added_at = chrono::Utc::now();
     let result = badge.result as u8;
     let query = sqlx::query!(
@@ -240,7 +264,7 @@ async fn add_badge(
         project_id,
         badge.archive_path,
     );
-    query.execute(&pool).await.unwrap();
+    query.execute(&pool).await?;
 
-    (StatusCode::OK, "")
+    Ok((StatusCode::OK, ""))
 }
