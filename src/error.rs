d06748b2d9eb5427b6bca86682fc0e173aad6dc5
Jørgen P. Tjernø <jorgen@tjer.no>
2022-05-19 15:55:14 -0700

    Log ISEs to `tracing`

--- src/error.rs
+++ src/error.rs
@@ -2,12 +2,14 @@ use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
+use tracing::error;
 
 #[derive(Debug)]
 pub struct AppError(pub anyhow::Error);
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
+        error!("Internal server error: {:?}", self.0);
         (StatusCode::INTERNAL_SERVER_ERROR, self.0.to_string()).into_response()
     }
 }
