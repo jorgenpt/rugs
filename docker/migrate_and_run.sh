be1236d2105576ec07b1cb60685c17a40474cb97
Jørgen P. Tjernø <jorgenpt@gmail.com>
2023-07-09 14:38:00 -0700

    Docker: Error out if sqlx migrations fail

--- docker/migrate_and_run.sh
+++ docker/migrate_and_run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 export DATABASE_URL="sqlite:$1"
-/app/sqlx database create
-/app/sqlx migrate run
+/app/sqlx database create || exit 1
+/app/sqlx migrate run || exit 1
 exec /app/rugs_metadata_server "--database=$1"
